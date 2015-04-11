67e42e58b82555d415b8f5a25a9ebe69ea15b04c
Arnoo_s <aaron.schriever@gmail.com>
2015-04-11 23:41:37 +0930

    start of lesson 1, add appends in JS, get rid of helloWorld

---
+++ js/resumeBuilder.js
@@ -0,0 +1,5 @@
+$("#main").append("Aaron Schriever"); 
+var awesomeThoughts = "I am Aaron and I am Awesome!";
+console.log(awesomeThoughts);
+var funThoughts = awesomeThoughts.replace(' Awesome', 'Fun');
+$("#main").append(funThoughts);
