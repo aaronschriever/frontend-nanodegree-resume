b03c1607269f857914e99bb5a3c66aac40642664
Arnoo_s <aaron.schriever@gmail.com>
2015-04-12 12:30:27 +0930

    add name and roles.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,6 @@
-$("#main").append("Aaron Schriever"); 
-var awesomeThoughts = "I am Aaron and I am Awesome!";
-console.log(awesomeThoughts);
-var funThoughts = awesomeThoughts.replace(' Awesome', 'Fun');
-$("#main").append(funThoughts);
+var formattedRole = HTMLheaderRole.replace("%data%", "Android, C# and web developer");
+$("#header").prepend(formattedRole);
+console.log(formattedRole);
+var formattedName = HTMLheaderName.replace("%data%", "Aaron Schriever");
+$("#header").prepend(formattedName);
+console.log(formattedName);
